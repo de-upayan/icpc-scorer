4a9629703cd2d6216e89b545d2d40a942225c1c5
Upayan De <96875426+de-upayan@users.noreply.github.com>
2023-07-01 11:52:35 +0530

    Completed ScorecardPanel

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.lang.*;
 import java.time.*;
 import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.border.*;
 
 enum Verdict 
 {
@@ -22,21 +23,25 @@ class Submission
         Instant submissionTime;
 }
 
-class SubmissionList
-{
-        ArrayList < Submission > list;
-}
-
 class Problem
 {
-        int ProblemID;
+        int problemID;
+        int numberOfAttempts;
+        String problemSymbol;
         boolean isSolved;
-        int lastSubmissionID;
-}
-
-class ProblemList
-{
-        ArrayList < Problem > list;
+        Instant solveTime;
+        
+        Problem (
+                int problemID,
+                String problemSymbol
+        )
+        {
+                this.problemID = problemID;
+                this.numberOfAttempts = 0;
+                this.problemSymbol = problemSymbol;
+                this.isSolved = false;
+                this.solveTime = null;
+        }
 }
 
 class Contest
@@ -49,8 +54,9 @@ class Contest
         Duration timePenalty;
         final Duration penaltyPerNonAC;
         final Duration contestDuration;
-        ProblemList problems;
-        SubmissionList submissions;
+        Instant startTime;
+        Problem[] problems;
+        ArrayList < Submission > submissions;
 
         Contest(
                 int numberOfProblems,
@@ -68,15 +74,23 @@ class Contest
                 this.timePenalty = Duration.ZERO;
                 this.penaltyPerNonAC = penaltyPerNonAC;
                 this.contestDuration = contestDuration;
-                this.problems = new ProblemList();
-                this.submissions = new SubmissionList();
+                this.startTime = Instant.now();
+                
+                this.problems = new Problem[numberOfProblems];
+                for (int i = 0; i < numberOfProblems; i++)
+                {
+                        problems[i] = new Problem(i, Character.toString('A' + i));
+                }
+                
+                this.submissions = new ArrayList < Submission > ();
         }
 }
 
 class ContestWindow extends JFrame
 {
         Contest contest;
-
+        
+        // source: https://stackoverflow.com/a/12731354
         public static void changeFont(Component component, Font font)
         {
                 component.setFont(font);
@@ -88,6 +102,20 @@ class ContestWindow extends JFrame
                         }
                 }
         }
+        
+        // source: https://stackoverflow.com/a/266846
+        public static String formatDuration(Duration duration)
+        {
+                long seconds = duration.getSeconds();
+                long absSeconds = Math.abs(seconds);
+                String positive = String.format(
+                        "%d:%02d:%02d",
+                        absSeconds / 3600,
+                        (absSeconds % 3600) / 60,
+                        absSeconds % 60
+                );
+                return (seconds < 0 ? "-" + positive : positive);
+        }
 
         Contest showContestSetupDialog()
         {
@@ -97,6 +125,7 @@ class ContestWindow extends JFrame
                 
                 JLabel problemsLabel = new JLabel("Total Problems (A - ?)");
                 JComboBox problemsComboBox = new JComboBox("ABCDEFGHIJKLMOPQRSTUVWXYZ".split(""));
+                problemsComboBox.setSelectedIndex(4);
                 problemsComboBox.setEditable(false);
                 
                 JLabel durationLabel = new JLabel("Duration");
@@ -110,6 +139,7 @@ class ContestWindow extends JFrame
                         hoursComboBoxOptions[i] = i;
                 }
                 JComboBox hoursComboBox = new JComboBox(hoursComboBoxOptions);
+                hoursComboBox.setSelectedIndex(3);
                 hoursComboBox.setEditable(false);
                 
                 JLabel minutesLabel = new JLabel("Minutes");
@@ -134,7 +164,7 @@ class ContestWindow extends JFrame
                         penaltyComboBoxOptions[i] = 5 * i;
                 }
                 JComboBox penaltyComboBox = new JComboBox(penaltyComboBoxOptions);
-                penaltyComboBox.setSelectedItem(penaltyComboBoxOptions[4]);
+                penaltyComboBox.setSelectedIndex(4);
                 penaltyComboBox.setEditable(false);
                 
                 JLabel teamNameLabel = new JLabel("Team Name");
@@ -225,10 +255,160 @@ class ContestWindow extends JFrame
         {
                 contest = showContestSetupDialog();
                 setTitle("Contest - " + contest.teamName);
+                
+                setLayout(new BorderLayout());
+                
+                add(new TopPanel(), BorderLayout.NORTH);
+                add(new SubmissionPanel(), BorderLayout.CENTER);
+                add(new BottomPanel(), BorderLayout.SOUTH);
 
                 setSize(960, 540);
+                setResizable(false);
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 setVisible(true);
+        }
+        
+        private class ProblemGUIEntity extends JPanel
+        {
+                final Problem problem;
+                JLabel symbolLabel, scoreLabel, timeSolvedLabel;
+                JPanel solvedPanel;
+                
+                ProblemGUIEntity(Problem problem)
+                {                                                
+                        this.problem = problem;
+                        
+                        symbolLabel = new JLabel(problem.problemSymbol);
+                        symbolLabel.setHorizontalAlignment(JLabel.CENTER);
+                        changeFont(symbolLabel, symbolLabel.getFont().deriveFont(Font.BOLD, 20));
+                        
+                        scoreLabel = new JLabel("-");
+                        scoreLabel.setHorizontalAlignment(JLabel.CENTER);
+                        
+                        timeSolvedLabel = new JLabel("-");
+                        timeSolvedLabel.setHorizontalAlignment(JLabel.CENTER);
+                        
+                        setLayout(new GridLayout(3, 1));
+                        setBorder(new EmptyBorder(10, 5, 10, 5));
+                        
+                        add(symbolLabel);
+                        add(scoreLabel);
+                        add(timeSolvedLabel);
+                }
+                
+                public void update()
+                {
+                        if (problem.isSolved)
+                        {
+                                setBackground(Color.green);
+                                
+                                scoreLabel.setText("1 (" + (problem.numberOfAttempts - 1) +")");
+                                timeSolvedLabel.setText(
+                                        formatDuration(Duration.between(contest.startTime, problem.solveTime))
+                                );
+                        }
+                        else if (problem.numberOfAttempts > 0)
+                        {
+                                setBackground(Color.red);
+                                
+                                scoreLabel.setText("0 (" + problem.numberOfAttempts +")");
+                        }
+                }
+        }
+        
+        private class ScorecardPanel extends JPanel
+        {
+                JPanel teamDetailsPanel, scorePanel;
+                JLabel teamNameLabel, institutionLabel, scoreLabel, timePenaltyLabel;
+                ProblemGUIEntity[] problemList;
+                
+                ScorecardPanel()
+                {
+                        setLayout(new GridLayout(1, 2 + contest.numberOfProblems));
+                        
+                        teamDetailsPanel = new JPanel();
+                        teamDetailsPanel.setLayout(new GridLayout(2, 1));
+                        /* teamDetailsPanel begins */
+                        teamNameLabel = new JLabel(contest.teamName);
+                        teamNameLabel.setHorizontalAlignment(JLabel.CENTER);
+                        changeFont(teamNameLabel, teamNameLabel.getFont().deriveFont(Font.BOLD, 15));
+                        teamDetailsPanel.add(teamNameLabel);
+
+                        
+                        institutionLabel = new JLabel(contest.institution);
+                        institutionLabel.setHorizontalAlignment(JLabel.CENTER);
+                        changeFont(institutionLabel, institutionLabel.getFont().deriveFont(Font.ITALIC));
+                        teamDetailsPanel.add(institutionLabel);
+                        /* teamDetailsPanel ends */
+                        teamDetailsPanel.setBorder(new EmptyBorder(10, 10, 10, 5));
+                        add(teamDetailsPanel);
+                        
+                        scorePanel = new JPanel();
+                        scorePanel.setLayout(new GridLayout(2, 1));
+                        /* scorePanel begins */
+                        scoreLabel = new JLabel(Integer.toString(contest.score));
+                        scoreLabel.setHorizontalAlignment(JLabel.CENTER);
+                        scorePanel.add(scoreLabel);
+                        
+                        timePenaltyLabel = new JLabel(formatDuration(contest.timePenalty));
+                        timePenaltyLabel.setHorizontalAlignment(JLabel.CENTER);
+                        scorePanel.add(timePenaltyLabel);
+                        /* scorePanel ends */
+                        scorePanel.setBorder(new EmptyBorder(10, 5, 10, 5));
+                        add(scorePanel);
+                        
+                        problemList = new ProblemGUIEntity[contest.numberOfProblems];
+                        for (int i = 0; i < contest.numberOfProblems; i++)
+                        {
+                                problemList[i] = new ProblemGUIEntity(contest.problems[i]);
+                                add(problemList[i]);
+                        }
+                }
+        }
+        
+        private class TimerPanel extends JPanel
+        {
+                
+        }
+        
+        private class TopPanel extends JPanel
+        {
+                JScrollPane scorecardPanelScrollPane;
+                
+                TopPanel()
+                {
+                        setLayout(new BorderLayout());
+                        setBorder(new EmptyBorder(10, 10, 5, 5));
+                        
+                        scorecardPanelScrollPane = new JScrollPane(new ScorecardPanel());
+                        scorecardPanelScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);  
+                        scorecardPanelScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+                        scorecardPanelScrollPane.setPreferredSize(new Dimension(640, 120));
+                        scorecardPanelScrollPane.setBorder(new EtchedBorder());
+                        add(scorecardPanelScrollPane, BorderLayout.WEST);
+                        
+                        add(new TimerPanel(), BorderLayout.EAST);
+                }
+        }
+        
+        private class SubmissionPanel extends JPanel
+        {
+                
+        }
+        
+        private class InputPanel extends JPanel
+        {
+                
+        }
+        
+        private class ButtonPanel extends JPanel
+        {
+                
+        }
+        
+        private class BottomPanel extends JPanel
+        {
+                
         }
 }
 
